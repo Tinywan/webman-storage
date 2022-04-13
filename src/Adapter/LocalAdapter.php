68a314adf83ea1568c89ed96770339f2872412f6
万少波 <756684177@qq.com>
2022-04-13 22:08:28 +0800

    local url

--- src/Adapter/LocalAdapter.php
+++ src/Adapter/LocalAdapter.php
@@ -25,7 +25,8 @@ class LocalAdapter extends AdapterAbstract
         if (!$this->createDir($basePath)) {
             throw new StorageException('文件夹创建失败，请核查是否有对应权限。');
         }
-        $baseUrl = $this->config['domain'].$this->config['uri'].str_replace(DIRECTORY_SEPARATOR, '/', $$this->config['dirname']).'/';
+
+        $baseUrl = $this->config['domain'].$this->config['uri'].str_replace(DIRECTORY_SEPARATOR, '/', $this->config['dirname']).DIRECTORY_SEPARATOR;
         foreach ($this->files as $key => $file) {
             $uniqueId = hash_file('sha1', $file->getPathname());
             $saveFilename = $uniqueId.'.'.$file->getUploadExtension();
@@ -50,28 +51,20 @@ class LocalAdapter extends AdapterAbstract
 
     /**
      * @desc: createDir 描述
-     *
-     * @author Tinywan(ShaoBo Wan)
      */
     protected function createDir(string $path): bool
     {
-        // 判断传过来的$path是否已是目录，若是，则直接返回true
         if (is_dir($path)) {
             return true;
         }
 
-        // 走到这步，说明传过来的$path不是目录
-        // 判断其上级是否为目录，否，则创建上级目录
         $parent = dirname($path);
         if (!is_dir($parent)) {
-            // 创建上级目录
             if (!$this->createDir($parent)) {
-                // 创建失败，返回 false
                 return false;
             }
         }
 
-        // 走到这步，说明上级目录已创建成功，则直接接着创建当前目录，并把创建的结果返回
         return mkdir($path);
     }
 }
