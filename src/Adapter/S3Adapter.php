431fa073da65a306fea244092a02a5460bf1579a
Tinywan <756684177@qq.com>
2024-09-06 20:25:04 +0800

    feat: s3 beat

--- src/Adapter/S3Adapter.php
+++ src/Adapter/S3Adapter.php
@@ -90,4 +90,69 @@ class S3Adapter extends AdapterAbstract
         return $result;
     }
 
+    /**
+     * @desc 上传服务端文件
+     * @param string $file_path
+     * @return array
+     * @author Tinywan(ShaoBo Wan)
+     */
+    public function uploadServerFile(string $file_path): array
+    {
+        $file = new \SplFileInfo($file_path);
+        if (!$file->isFile()) {
+            throw new StorageException('不是一个有效的文件');
+        }
+
+        $uniqueId = hash_file($this->algo, $file->getPathname());
+        $object = $this->config['dirname'] . $this->dirSeparator . $uniqueId . '.' . $file->getExtension();
+
+        $result = [
+            'origin_name' => $file->getRealPath(),
+            'save_path' => $object,
+            'url' => $this->config['domain'] . $this->dirSeparator . $object,
+            'unique_id' => $uniqueId,
+            'size' => $file->getSize(),
+            'extension' => $file->getExtension(),
+        ];
+
+        $this->getInstance()->putObject([
+            'Bucket' => $this->config['bucket'],
+            'Key' => $object,
+            'Body' => fopen($file->getPathname(), 'rb'),
+        ]);
+
+        return $result;
+    }
+
+    /**
+     * @desc: 上传Base64
+     * @param string $base64
+     * @param string $extension
+     * @return array
+     * @author Tinywan(ShaoBo Wan)
+     */
+    public function uploadBase64(string $base64, string $extension = 'png'): array
+    {
+        $base64 = explode(',', $base64);
+        $uniqueId = date('YmdHis') . uniqid();
+        $object = $this->config['dirname'] . $this->dirSeparator . $uniqueId . '.' . $extension;
+
+        $this->getInstance()->putObject([
+            'Bucket' => $this->config['bucket'],
+            'Key' => $object,
+            'Body' => base64_decode($base64[1]),
+        ]);
+
+        $imgLen = strlen($base64['1']);
+        $fileSize = $imgLen - ($imgLen / 8) * 2;
+
+        return [
+            'save_path' => $object,
+            'url' => $this->config['domain'] . $this->dirSeparator . $object,
+            'unique_id' => $uniqueId,
+            'size' => $fileSize,
+            'extension' => $extension,
+        ];
+    }
+
 }
